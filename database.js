b0bb3687d361a00edf4f9f0d00b31d50fa70a35c
brendacsantos <brendac.santos2@gmail.com>
2020-06-02 00:45:54 +0000

    ok

---
+++ database.js
@@ -0,0 +1,78 @@
+// inserir o comando para abrir o banco: yarn add pg 
+
+const Pool = require('pg').Pool;
+
+// 1 - Abrir conexão
+// 2 - Execurar comando SQL (select, insert....)
+// 3 - Fechar conexão
+
+const pool = new Pool ({
+    user: 'llnplyrjxztzos',
+    password:'060911fea32dfe8251c01d614b98dfd450103bb4e466402c455f23233ce4ec1f',
+    host:'ec2-34-232-147-86.compute-1.amazonaws.com',
+    database:'d9v7p8178vglef',
+    port: 5432,
+    ssl: {rejectUnauthorized: false}
+});
+
+/* const sqlCreate = `
+    DROP TABLE banho
+    CREATE TABLE IF NOT EXISTS banho
+    (
+        ID serial primary key,
+        nomedog varchar(50) not null,
+        peso int not null default 0,
+        valor decimal,
+        status varchar(20)
+    )
+`;
+
+//pool.query(sqlCreate, function(error,result) {
+//    if(error)
+//     throw error
+//     console.log('Tabela criada com sucesso!');
+});*/
+
+module.exports = {
+
+async insert (nomedog, peso, valor,status){
+    const sql = `INSERT INTO banho (nomedog, peso, valor, status) VALUES ($1, $2, $3)`;
+    
+    const result = await pool.query(sql, [nomedog, peso, valor,status]);
+    return result.rowCount;
+},
+
+async select (){
+    const sql = `SELECT * FROM banho`;
+    
+    const result = await pool.query(sql);
+    return result.rows;
+},
+
+  async  delete(nomedog){
+
+        const sql = `DELETE FROM banho WHERE nomedog = $1`;
+        
+        const result = await pool.query(sql, [nomedog]);
+        return result.rowCount;
+},
+
+async  update(nomedog, peso, valor, status){
+
+        const sql = `UPDATE banho
+        SET peso = $2, valor = $3, status= $4
+        WHERE nomedog = $1`;
+        
+        console.log(peso)
+     const result = await pool.query(sql, [nomedog, peso, valor, status]);
+        return result.rowCount;
+ },
+
+}
+
+/*
+async  read(){
+    const sql = 'SELECT * FROM banho '
+    const result = await pool.query(sql);
+    return result.rows;
+     }    */
--- server.js
+++ server.js
@@ -1,9 +1,48 @@
+
 const express = require('express');
-
+const database = require('./database');
+//const cors = require('cors');
 const server = express();
+//server.use(cors());
+server.use(express.json());
 
-server.get('/', function(request, response) {
-    response.send('Hello World!');
+
+server.get('/', async function(request, response) {
+    const dados = await database.select();
+    return response.json(dados);
+})
+
+
+server.post('/', async function(request, response) {
+
+    const nomedog = request.body.nomedog;
+    const peso = request.body.peso;
+    const valor = request.body.valor;
+    const status= request.body.status;
+
+    const result = await database.insert(nomedog, peso, valor, status);
+
+    return response.status(204).send();
+})
+
+server.put('/', async function(request, response) {
+
+    const nomedog = request.body.nomedog; 
+    const peso = request.body.peso;
+    const valor = request.body.valor;
+    const status= request.body.status;
+
+    const result = await database.update(nomedog, peso, valor, status);
+
+    return response.status(204).send();
+})
+
+server.delete('/', async function(request, response) {
+
+    const nomedog = request.body.nomedog;
+    const result = await database.delete(nomedog);
+
+    return response.status(204).send();
 })
 
 server.listen(process.env.PORT || 3000);
